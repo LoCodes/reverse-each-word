f79f9e47a4327f338ba1bd0ed6f1414b3ef5ff4a
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-19 18:53:57 -0800

    commit tree 1abedd2f29c54b641ecb2f94a60b15667179dbce

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence1)
+    sentence1.split.collect {|word| word.reverse}.join(" ")
+end 
+
+
+# #reverse_each_word
+#  reverses all the words in a string without reversing the order of the words
+#  reverses all the words in another string without reversing the order of the words
+#  uses collect
+
